03dab714b1b971e3944724e4965cda49893d24ee
arinokdawood-commits <arinokdawood@gmail.com>
2026-04-07 22:10:36 +0500

    Add files via upload

--- main2.py
+++ main2.py
@@ -1,12 +1,20 @@
-def isEvenOdd( n) :
-    if (n ^ 1 == n + 1) :
-        return True
-    else :
-        return False 
+def printTwoOdd (arr, size):
+    xorof2 = arr[0]
+    x = 0
+    y = 0
+    for i in range(1, size):
+        xorof2 = xorof2 ^ arr[i]
+    setbit = xorof2 & ~ (xorof2 - 1) 
+    for i in range(size):
+        if(arr[i] & setbit):
+            x = x ^ arr[i]
+        else:
+            y = y ^ arr[i]
+    print("The two ODD elements are" , x , "&" , y)
+arr = []
+arr_size = int(input("Enter size of the array :"))
+for i in range(0, arr_size):
+    z = int(input("Enter element :"))
+    arr.append(z)
+printTwoOdd(arr, arr_size)           
     
-number = int(input("Enter your number: "))
-
-if isEvenOdd(number):
-        print(number ," is even")
-else:
-     print(number , "is odd")        
